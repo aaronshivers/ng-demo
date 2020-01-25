1b254becc43943decda9bfcfbc061240a642c0a7
aaronshivers <aaron@moonmail.org>
2020-01-25 15:52:42 -0600

    add authReducer to app reducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,14 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { authReducer } from '../auth/reducers/index';
 
 export interface State {
 
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  auth: authReducer
 };
 
 
