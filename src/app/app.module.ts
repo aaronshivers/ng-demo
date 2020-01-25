7272bee74e92d4e49be4086bfc1993397cb13051
aaronshivers <aaron@moonmail.org>
2020-01-25 09:05:02 -0600

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
