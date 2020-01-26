78f31a8414eac84a361a9367ae56113e7a25dc89
aaronshivers <aaron@moonmail.org>
2020-01-26 10:14:58 -0600

    toggle menu on loggedIn state

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { AuthState } from '../auth/auth.reducer';
 import { logout } from '../auth/auth.actions';
+import { isLoggedIn, isLoggedOut } from '../auth/auth.selectors';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: [ './nav.component.scss' ],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -23,6 +27,11 @@ export class NavComponent {
     private breakpointObserver: BreakpointObserver,
     private store: Store<AuthState>,
   ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedIn));
+    this.isLoggedOut$ = this.store.pipe(select(isLoggedOut));
+  }
 
   onLogout() {
     this.store.dispatch(logout());
